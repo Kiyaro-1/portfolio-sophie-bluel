6f313dfc61c8e0cd33617dc34d81111e0cb01a87
Yassine Gamrani <kiyaroyass123@gmail.com>
2024-03-15 15:19:37 +0000

    Added: First modal to manage modifying works + functionnal works deletion from the modal

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -6,15 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const categories = document.getElementById("categories");
   const loginbtn = document.getElementById("loginbtn");
   const logoutbtn = document.getElementById("logoutbtn");
+  const modify = document.getElementById("modify");
 
   // If there is a valid token, then the user is logged in
   if (token) {
     categories.style.display = "none";
     loginbtn.style.display = "none";
     logoutbtn.style.display = "block";
+    modify.style.display = "block";
   } else {
     loginbtn.style.display = "block";
     logoutbtn.style.display = "none";
+    modify.style.display = "none";
   }
 });
 
@@ -101,4 +104,125 @@ filtres.forEach(function (filtre) {
           });
     generateworks(filterimages);
   });
+});
+
+//Handle gallery modal
+let modal = null;
+
+//Open the modal
+const openmodal = (e) => {
+  e.preventDefault();
+  const target = document.getElementById("modal");
+  target.classList.remove("hidden");
+  target.removeAttribute("aria-hidden");
+  target.setAttribute("aria-modal", "true");
+  modal = target;
+  modal.addEventListener("click", closemodal);
+  modal.querySelector(".js-modal-close").addEventListener("click", closemodal);
+  modal
+    .querySelector(".js-modal-stop")
+    .addEventListener("click", stopPropagation);
+
+  //Handle Delete Work
+  const apiUrl = "http://localhost:5678/api/works";
+
+  async function deleteWork(index) {
+    const workToDelete = works[index];
+    const token = localStorage.getItem("accessToken");
+
+    //check for authorization
+    if (!token) {
+      console.error("Authorization token not found.");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${apiUrl}/${workToDelete.id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Update works by removing the deleted work
+      works.splice(index, 1);
+
+      // Update the modal content
+      generateworksmodal(works);
+
+      // Update the main page content
+      generateworks(works);
+    } catch (error) {
+      console.error("Error deleting work:", error);
+    }
+  }
+
+  //generate works in the modal
+  function generateworksmodal(works) {
+    const divgallerymodal = document.querySelector(".gallery-modal");
+    divgallerymodal.innerHTML = "";
+    for (let i = 0; i < works.length; i++) {
+      const containerElement = document.createElement("div");
+      const imageElement = document.createElement("img");
+      const deleteIcon = document.createElement("i");
+
+      containerElement.className = "work-container";
+      imageElement.src = works[i].imageUrl;
+      deleteIcon.className = "fa-solid";
+      deleteIcon.classList.add("fa-trash-can", "fa-sm");
+
+      divgallerymodal.appendChild(containerElement);
+      containerElement.appendChild(imageElement);
+      containerElement.appendChild(deleteIcon);
+      deleteIcon.addEventListener("click", function () {
+        const workIndex = i; // Store the current index
+        deleteWork(workIndex); // deleteWork function with the stored index
+      });
+    }
+  }
+  generateworksmodal(works);
+};
+
+//Close gallery modal
+/**
+ *
+ * @param {DOMEvent} e
+ * @returns
+ */
+const closemodal = (e) => {
+  if (modal === null) return;
+  e.preventDefault();
+  const target = document.getElementById("modal");
+  target.classList.add("hidden");
+  target.setAttribute("aria-hidden", "true");
+  target.removeAttribute("aria-modal");
+  target.removeEventListener("click", closemodal);
+  target
+    .querySelector(".js-modal-close")
+    .removeEventListener("click", closemodal);
+  target
+    .querySelector(".js-modal-stop")
+    .removeEventListener("click", stopPropagation);
+  modal = null;
+};
+
+const stopPropagation = (e) => {
+  e.stopPropagation();
+};
+
+document.querySelectorAll(".js-modal").forEach((a) => {
+  a.addEventListener("click", openmodal);
+});
+
+//handle Escape key closes modal
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    closemodal(e);
+    closemodal2(e);
+  }
 });
