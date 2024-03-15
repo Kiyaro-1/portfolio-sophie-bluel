3962ec5c8fb9216a46c6ba626d24cc69c1b6a8c5
Yassine Gamrani <kiyaroyass123@gmail.com>
2024-03-15 14:44:25 +0000

    Added: Working filtres to filter works by category

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -23,3 +23,54 @@ function generateworks(works) {
 
 //first Works display
 generateworks(works);
+
+//recover categories
+const btns = await fetch("http://localhost:5678/api/categories").then((works) =>
+  works.json()
+);
+
+//generate categories
+function generatebuttons(btns) {
+  const divfiltres = document.querySelector(".filtres");
+  for (let i = 0; i < btns.length; i++) {
+    const titleElement = document.createElement("h3");
+    const linkElement = document.createElement("a");
+
+    linkElement.innerText = btns[i].name;
+    linkElement.id = btns[i].id;
+
+    divfiltres.appendChild(titleElement);
+    titleElement.appendChild(linkElement);
+    linkElement.classList.add("button1");
+  }
+}
+
+//first categories display
+generatebuttons(btns);
+
+//filtre buttons
+const filtres = document.querySelectorAll(".button1");
+for (let i = 0; i < filtres.length; i++) {
+  filtres[i].setAttribute("data-category", i);
+}
+
+//filtre buttons style
+filtres.forEach(function (filtre) {
+  filtre.addEventListener("click", () => {
+    for (let i = 0; i < filtres.length; i++) {
+      filtres[i].className = filtres[i].className.replace(" selected", "");
+    }
+    filtre.classList.add("selected");
+    const divgallery = document.querySelector(".gallery");
+    divgallery.innerHTML = "";
+    //Filter works
+    const categoryId = filtre.getAttribute("data-category");
+    const filterimages =
+      categoryId === "0"
+        ? works
+        : works.filter(function (work) {
+            return work.categoryId === parseInt(categoryId);
+          });
+    generateworks(filterimages);
+  });
+});
